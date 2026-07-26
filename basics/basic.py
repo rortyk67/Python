f4259a03495949fa6a527cb2b241322e75a7c224
rortyk67 <ringowotaberarenai@gmail.com>
2026-07-26 15:27:35 +0900

    文字列のfind・rfindとf-stringの練習を追加

--- basics/basic.py
+++ basics/basic.py
@@ -1,1 +1,10 @@
 print("Hello World Python!")
+
+s = 'My name is Mike. Hi Mike.'
+print(s)
+print(s.find('Mike'))
+print(s.rfind('Mike'))
+
+name = 'kenta'
+family = 'nakamura'
+print(f'My name is {name} {family}. watashi ha {family} {name}')
